3b53794fe5aab6d7352134a186f34282047b124b
Catalina-Marinica <57720538+Catalina-Marinica@users.noreply.github.com>
2020-09-17 15:07:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const button = document.querySelector(".my-btn")
+const body = document.querySelector("body")
+
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+button.addEventListener("click", function (event) {
+  body.style.backgroundColor = getRandomColor()
+})
+
+
+
+
+
+
+
